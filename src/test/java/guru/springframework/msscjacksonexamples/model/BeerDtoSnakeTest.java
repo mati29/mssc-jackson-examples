4d7f05b0205fc47feda947e51182ed96b618c723
Mati <mering20@gmail.com>
2020-09-03 12:19:21 +0200

    add naming stategy

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -0,0 +1,35 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.io.IOException;
+
+@ActiveProfiles("snake")
+@JsonTest
+public class BeerDtoSnakeTest extends BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSerializeDto() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString);
+    }
+
+    @Test
+    void testDeserializeDto() throws IOException {
+        String json = "{\"id\":\"819cd63b-c304-422f-a81a-db8b9524a3e2\",\"beer_name\":\"BeerName\",\"beer_style\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"created_date\":\"2020-09-03T12:17:15.7178264+02:00\",\"last_updated_date\":\"2020-09-03T12:17:15.7188254+02:00\"}";
+        BeerDto dto = objectMapper.readValue(json, BeerDto.class);
+
+        System.out.println(dto);
+    }
+}
---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+import java.io.IOException;
+
+@JsonTest
+class BeerDtoTest extends BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSerializeDto() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString);
+    }
+
+    @Test
+    void testDeserializeDto() throws IOException {
+        String json = "{\"id\":\"c7f8e3c3-64b4-4ea4-8228-7d5db34ecd67\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-09-03T11:53:39.0952423+02:00\",\"lastUpdatedDate\":\"2020-09-03T11:53:39.0962422+02:00\"}";
+
+        BeerDto dto = objectMapper.readValue(json, BeerDto.class);
+
+        System.out.println(dto);
+    }
+}
